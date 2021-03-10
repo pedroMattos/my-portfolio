3befd56ab70e4408dd1e6f25f5260771a30d627c
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-10 20:11:13 -0300

    banner estrutura

---
+++ src/components/header/banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner">
+    <div class="text-banner">
+      <h1 class="white centralizer">Pedro Mattos</h1>
+      <div class="lines"></div>
+      <h2 class="white centralizer">Code Designer</h2>
+    </div>
+    <div class="overlay"></div>
+    <img id="img-banner" src="@/assets/images/banner.jpg" alt="Banner Home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'banner'
+}
+</script>
