564ad65fb247e944ddbac4fb1019d534214839c5
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-10 00:09:43 -0300

    test tipography

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Base</h1>
+  <div class="header">
+    <h1>Base</h1>
+    <h2>About</h2>
+    <h3>Personal details</h3>
+    <p class="bold"><span>Hello! I'm Pedro Mattos.</span> Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p class="small">Lorem ipsum dolor sit amet</p>
+  </div>
 </template>
 
 <style lang="scss">
