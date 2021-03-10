8191989f79cad2cb515a5d8b2d2162fa015d1d53
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-10 20:05:09 -0300

    mudando estrutura base

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="header">
-    <h1>Base</h1>
-    <h2>About</h2>
-    <h3>Personal details</h3>
-    <p class="bold"><span>Hello! I'm Pedro Mattos.</span> Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <p class="small">Lorem ipsum dolor sit amet</p>
-  </div>
+  <router-view/>
 </template>
 
-<style lang="scss">
-</style>
+<script>
+export default {
+  name: 'root',
+};
+</script>
