504b3cd57765c9a267e97b20f5e65644fc8284d2
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-10 23:32:37 -0300

    avatar estrutura

---
+++ src/components/header/avatar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="avatar-wrapper">
+    <div class="external-circle">
+      <div class="internal-circle">
+        <p class="centralizer">
+          <img src="@/assets/images/avatar.jpg" alt="Foto Pedro">
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'avatar',
+};
+</script>
