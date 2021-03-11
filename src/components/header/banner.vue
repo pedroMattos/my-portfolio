5b85f88bd07fce871a3bad520e24274a8788b857
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-10 23:32:49 -0300

    import avatar no banner

--- src/components/header/banner.vue
+++ src/components/header/banner.vue
@@ -5,13 +5,19 @@
       <div class="lines"></div>
       <h2 class="white centralizer">Code Designer</h2>
     </div>
+    <avatar-photo></avatar-photo>
     <div class="overlay"></div>
     <img id="img-banner" src="@/assets/images/banner.jpg" alt="Banner Home">
   </div>
 </template>
 
 <script>
+import avatar from './avatar.vue'
+
 export default {
-  name: 'banner'
+  name: 'banner',
+  components: {
+    'avatar-photo': avatar,
+  },
 }
 </script>
