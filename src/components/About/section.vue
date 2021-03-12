28f1a8314eeafd2ddae7931f4f330e45163b3780
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-11 22:46:49 -0300

    novo componente

---
+++ src/components/About/section.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container section">
+    <h2 class="centralizer no-bottom-m">About me</h2>
+    <div class="lines section-width"></div>
+    <br><br>
+    <p><span>Hello! I'm Pedro Mattos.</span> Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat.</p>
+      <br><br>
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <personal-details></personal-details>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import peronalDetails from './personalDetails.vue';
+
+export default {
+  name: 'section-about',
+  components: {
+    'personal-details': peronalDetails,
+  },
+}
+</script>
