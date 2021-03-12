f9bc7979de157ae8a7021f51dd0d28cc88d7a788
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-11 22:46:58 -0300

    nova estrutura

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
     <banner-header></banner-header>
-    <h1>Base</h1>
-    <h2>About</h2>
-    <h3>Personal details</h3>
-    <p class="bold"><span>Hello! I'm Pedro Mattos.</span> Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <p class="small">Lorem ipsum dolor sit amet</p>
+    <about-section></about-section>
   </div>
 </template>
 
 <script>
 import banner from '../components/header/banner.vue';
+import aboutSection from '../components/About/section.vue'
 
 export default {
   name: 'home',
   components: {
     'banner-header': banner,
+    'about-section': aboutSection,
   },
 };
 </script>
