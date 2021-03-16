c65fbc59d0dac8cc91b0fdfee39f0e5124152a78
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-15 21:12:59 -0300

    componente card

---
+++ src/components/About/cardInterests.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-interests">
+    <p class="centralizer">
+      <game-pad></game-pad>
+    </p>
+    <p class="centralizer small">
+      Games
+    </p>
+  </div>
+</template>
+
+<script>
+import gamepad from '../../assets/svg/gamepad';
+export default {
+  name: 'card-interests',
+  components: {
+    'game-pad': gamepad
+  }
+}
+</script>
