e65a501d612ed6a3fcc9fe2c2695b657c23e82f3
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-15 23:12:26 -0300

    importa section

---
+++ src/components/Skills/section.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner center">
+    <div class="text-banner">
+      <h2 class="white centralizer no-bottom-m">Skills</h2>
+      <div class="lines section-width"></div>
+    </div>
+    <avatar-photo></avatar-photo>
+    <div class="overlay"></div>
+    <img id="img-banner" src="@/assets/images/banner-skills.jpg" alt="Banner Home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'section-skills'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <banner-header></banner-header>
     <about-section></about-section>
+    <skills-section></skills-section>
   </div>
 </template>
 
 <script>
 import banner from '../components/header/banner.vue';
 import aboutSection from '../components/About/section.vue'
+import skillsSection from '../components/Skills/section.vue'
 
 export default {
   name: 'home',
   components: {
     'banner-header': banner,
     'about-section': aboutSection,
+    'skills-section': skillsSection
   },
 };
 </script>
