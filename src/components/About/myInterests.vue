e77a4bed89e4bbfed3c84ad2c7e01b2294ee371b
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-15 21:13:14 -0300

    nova estrutura

--- src/components/About/myInterests.vue
+++ src/components/About/myInterests.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
     <h3>My interests</h3>
+    <br>
+    <div class="row">
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+      <div class="col-md-2">
+        <card-interests></card-interests>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import cardInterests from './cardInterests';
 export default {
   name: 'my-interests',
+  components: {
+    'card-interests': cardInterests,
+  }
 }
 </script>
