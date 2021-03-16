3bd1bc6fa658df3ab5dec04797666c458a7224d1
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-15 22:04:03 -0300

    componente cta

---
+++ src/assets/cta/btnCta.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="button-cta">
+    <p class="centralizer">
+      Download Cv
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'call-to-action',
+
+  mounted() {
+    let btn = document.getElementsByClassName('button-cta')[0]
+    btn.addEventListener('mouseenter', () => {
+      btn.classList.add('enter')
+    })
+    btn.addEventListener('mouseout', () => {
+      btn.classList.remove('enter')
+      btn.classList.add('out')
+      setTimeout(() => {
+        btn.classList.remove('out')
+      }, 500);
+    })
+  },
+}
+</script>
