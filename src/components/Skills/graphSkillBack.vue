b1a0b8b10082787c769c0b14812e2013bf1fe4bc
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-16 22:27:56 -0300

    graficos de front e back

---
+++ src/components/Skills/graphSkillBack.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="backend">
+    <h3 class="centralizer white">Backend</h3>
+    <v-chart class="chart" :option="option" />
+  </div>
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { RadarChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, defineComponent } from "vue";
+use([
+  CanvasRenderer,
+  RadarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
+
+export default {
+  name: "graph-skills-back",
+  components: {
+    VChart,
+  },
+  data() {
+    return {
+      option: {
+        tooltip: {},
+        radar: {
+          // shape: 'circle',
+          name: {
+            textStyle: {
+              color: "#fff",
+              backgroundColor: "#D94169",
+              borderRadius: 3,
+              padding: [3, 5],
+            },
+          },
+          indicator: [
+            { name: "Express", max: 100 },
+            { name: "Websockets", max: 100 },
+            { name: "NodeJs", max: 100 },
+            { name: "Python", max: 100 },
+            { name: "PHP", max: 100 },
+            { name: "API Rest", max: 100 },
+          ],
+        },
+        series: [
+          {
+            name: "Backend Skills",
+            type: "radar",
+            data: [
+              {
+                value: [80, 70, 80, 80, 85, 80],
+                name: "Backend Skills",
+              },
+            ],
+            itemStyle: {
+              color: "#D94169",
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
---
+++ src/components/Skills/graphSkillFront.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="frontend">
+    <h3 class="centralizer white">Frontend</h3>
+    <v-chart class="chart" :option="option" />
+  </div>
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { RadarChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, defineComponent } from "vue";
+use([
+  CanvasRenderer,
+  RadarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
+
+export default {
+  name: "graph-skills-front",
+  components: {
+    VChart,
+  },
+  data() {
+    return {
+      option: {
+        tooltip: {},
+        radar: {
+          // shape: 'circle',
+          name: {
+            textStyle: {
+              color: "#fff",
+              backgroundColor: "#D94169",
+              borderRadius: 3,
+              padding: [3, 5],
+            },
+          },
+          indicator: [
+            { name: "HTML5", max: 100 },
+            { name: "CSS/SCSS", max: 100 },
+            { name: "JavaScript", max: 100 },
+            { name: "UI/UX", max: 100 },
+            { name: "VueJs", max: 100 },
+            { name: "ReactJs", max: 100 },
+          ],
+        },
+        series: [
+          {
+            name: "Frontend Skills",
+            type: "radar",
+            data: [
+              {
+                value: [80, 80, 95, 75, 85, 60],
+                name: "Frontend Skills",
+              },
+            ],
+            itemStyle: {
+              color: "#D94169",
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
