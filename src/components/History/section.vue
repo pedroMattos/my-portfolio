cd56d0f3ba74354a620c5dc0a6f7cfffde14b439
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-16 22:44:08 -0300

    section history

---
+++ src/components/History/section.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="no-margin-tp">
+    <div class="container">
+      <h2 class="centralizer no-bottom-m">My History</h2>
+      <div class="lines section-width"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'section-history'
+}
+</script>
