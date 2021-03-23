5e4188ae425430d844a7b6e83cbf8e2ce1885dd0
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-22 22:16:49 -0300

    estrutura dos cards

--- src/components/History/section.vue
+++ src/components/History/section.vue
@@ -3,6 +3,95 @@
     <div class="container">
       <h2 class="centralizer no-bottom-m">My History</h2>
       <div class="lines section-width"></div>
+
+
+      <br><br><br>
+      <div class="history-map">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 right-border">
+              <div class="card-history right">
+                <div class="history-title">
+                  <p>
+                    Full-Stack Developer
+                    <br>
+                    <span class="subtitle">Ledax Energia Inteligente</span>
+                  </p>
+                </div>
+                <div class="history-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 right-border"></div>
+            <div class="col-md-6">
+              <div class="card-history">
+                <div class="history-title">
+                  <p>
+                    Full-Stack Developer
+                    <br>
+                    <span class="subtitle">Ledax Energia Inteligente</span>
+                  </p>
+                </div>
+                <div class="history-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-md-6 right-border">
+              <div class="card-history right">
+                <div class="history-title">
+                  <p>
+                    Full-Stack Developer
+                    <br>
+                    <span class="subtitle">Ledax Energia Inteligente</span>
+                  </p>
+                </div>
+                <div class="history-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 right-border"></div>
+            <div class="col-md-6">
+              <div class="card-history">
+                <div class="history-title">
+                  <p>
+                    Full-Stack Developer
+                    <br>
+                    <span class="subtitle">Ledax Energia Inteligente</span>
+                  </p>
+                </div>
+                <div class="history-content">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
