a539d5b095973bbaae0ccd2a90c3a762f6d2b3e3
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-28 09:32:25 -0300

    section portfolio

---
+++ src/components/Portfolio/section.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <divi class="container">
+      <h2 class="centralizer no-bottom-m">My Portfolio</h2>
+      <div class="lines section-width"></div>
+    </divi>
+  </section>
+</template>
