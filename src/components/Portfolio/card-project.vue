e18acd92f97ee47d76f3bdc6f3951aef58df0658
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-04-24 15:39:47 -0300

    estrutura base

---
+++ src/components/Portfolio/card-project.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-body">
+    <div class="text-card">
+      <p class="centralizer">{{ dataProp }}</p>
+    </div>
+    <img src="@/assets/images/g2r_apresentacao.png" :alt="dataProp" srcset="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card-project',
+  props: ['dataProp'],
+}
+</script>
+
+<style lang="scss" scoped>
+ .card-body {
+   color: white;
+ }
+</style>
